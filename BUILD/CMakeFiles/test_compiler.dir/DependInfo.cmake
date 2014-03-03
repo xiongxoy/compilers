1cf3abcb8714692a5a6816d7cbb7d1b95718851d
xiongxoy <xiong.xoy@gmail.com>
2014-03-03 23:10:14 +0800

    Add Build

---
+++ BUILD/CMakeFiles/test_compiler.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/zx/Documents/workspace/projects/compilers/compilercompiler/haneda_cc.c" "/Users/zx/Documents/workspace/projects/compilers/BUILD/CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/compilercompiler/haneda_cc.c.o"
+  "/Users/zx/Documents/workspace/projects/compilers/include/haneda_util.c" "/Users/zx/Documents/workspace/projects/compilers/BUILD/CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/include/haneda_util.c.o"
+  "/Users/zx/Documents/workspace/projects/compilers/lex/haneda_lex.c" "/Users/zx/Documents/workspace/projects/compilers/BUILD/CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/lex/haneda_lex.c.o"
+  "/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/haneda_env.c" "/Users/zx/Documents/workspace/projects/compilers/BUILD/CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/haneda_env.c.o"
+  "/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/haneda_syntax.c" "/Users/zx/Documents/workspace/projects/compilers/BUILD/CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/haneda_syntax.c.o"
+  "/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/syntax_test.c" "/Users/zx/Documents/workspace/projects/compilers/BUILD/CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/syntax_test.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ BUILD/CMakeFiles/test_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/compilercompiler/haneda_cc.c.o"
+  "CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/include/haneda_util.c.o"
+  "CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/lex/haneda_lex.c.o"
+  "CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/haneda_syntax.c.o"
+  "CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/haneda_env.c.o"
+  "CMakeFiles/test_compiler.dir/Users/zx/Documents/workspace/projects/compilers/syntax_and_semantics/syntax_test.c.o"
+  "test_compiler.pdb"
+  "test_compiler"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/test_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
